063c99fafc98d506b04eec7f7ad6b1d223772add
mq61520 <qfncore@gmail.com>
2023-04-25 22:10:17 +0700

    22:10 25/4/2023

--- src/controller/recombeeController.js
+++ src/controller/recombeeController.js
@@ -45,6 +45,51 @@ exports.add_detail_view = (req, res) => {
   }
 };
 
+exports.add_purchase = (req, res) => {
+  console.log(req.body.userId, req.body.itemsId, req.body.timestamp);
+
+  const json = [];
+  for (let i = 0; i < req.body.itemsId.length; i++) {
+    json.push({
+      user_id: req.body.userId,
+      item_id: req.body.itemsId[i].info.sp_ma,
+      timestamp: req.body.timestamp,
+    });
+  }
+
+  try {
+    var rqs = recombee.requests;
+
+    var client = new recombee.ApiClient(
+      "ctu-dev",
+      "JOvz0KgwCjKhM9OUJwSmjb9nAoODSnIUChwbL7uErFgQ8g9XE1fc79YEGiUtwgM5",
+      { region: "ap-se" }
+    );
+
+    var requests = json.map((interaction) => {
+      var userId = interaction["user_id"];
+      var itemId = interaction["item_id"];
+      var time = interaction["timestamp"];
+
+      return new rqs.AddPurchase(userId, itemId, {
+        timestamp: time,
+        cascadeCreate: true,
+      });
+    });
+
+    client.send(new rqs.Batch(requests), (err, responses) => {
+      if (err) {
+        console.log(err);
+      }
+      console.log(responses);
+    });
+
+    res.send("ok");
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 exports.get_items_for_item = (req, res) => {
   try {
     var rqs = recombee.requests;
@@ -56,9 +101,9 @@ exports.get_items_for_item = (req, res) => {
     );
 
     client.send(
-      new rqs.RecommendItemsToItem(req.body.itemId, req.body.userId, 2),
+      new rqs.RecommendItemsToItem(req.body.itemId, req.body.userId, 3),
       (err, recommendations) => {
-        console.log(recommendations.recomms);
+        // console.log(recommendations.recomms);
         res.send(recommendations.recomms);
       }
     );
@@ -78,7 +123,7 @@ exports.get_items_for_user = (req, res) => {
     );
 
     client.send(
-      new rqs.RecommendItemsToUser(req.body.userId, 2),
+      new rqs.RecommendItemsToUser(req.body.userId, 3),
       (err, recommendations) => {
         console.log(recommendations.recomms);
         res.send(recommendations.recomms);
--- src/routes/recombeeRoute.js
+++ src/routes/recombeeRoute.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const recombeeController = require("../controller/recombeeController");
 
 router.post("/recombee/add_detail_view", recombeeController.add_detail_view);
+router.post("/recombee/add_purchase", recombeeController.add_purchase);
 router.post(
   "/recombee/get_items_for_item",
   recombeeController.get_items_for_item
